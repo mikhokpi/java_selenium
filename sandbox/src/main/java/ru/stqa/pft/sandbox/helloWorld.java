e7d3a5d4a69c41446281e75d4f2f0ac07ff3d9d2
mikhail.gerasim <54040496+mikhokpi@users.noreply.github.com>
2020-07-23 18:08:34 +0300

    add some function

--- sandbox/src/main/java/ru/stqa/pft/sandbox/helloWorld.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/helloWorld.java
@@ -2,6 +2,23 @@ package ru.stqa.pft.sandbox;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        hello("Vlad");
+        hello("user!");
+        double l = 5;
+        System.out.println("kvadrat = " + area(l));
+        double a = 4, b = 6;
+        System.out.println("pryamoygolnik = " + area(4,5));
+    }
+
+    public static void hello(String smb) {
+        System.out.println("Hello " + smb);
+    }
+
+    public static double area(double length) {
+        return length * length;
+    }
+
+    public static double area(double a, double b) {
+        return a * b;
     }
 }
