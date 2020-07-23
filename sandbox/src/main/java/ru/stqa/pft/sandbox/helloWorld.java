0ab059ba9625d326782edea343a47c650d6f3a98
mikhail.gerasim <54040496+mikhokpi@users.noreply.github.com>
2020-07-23 15:18:42 +0300

    class CamelCase and remove git plug

--- sandbox/src/main/java/ru/stqa/pft/sandbox/helloWorld.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/helloWorld.java
@@ -1,6 +1,6 @@
 package ru.stqa.pft.sandbox;
 
-public class helloWorld {
+public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
