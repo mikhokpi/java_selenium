d5f6ea2457f8ec811a1e433da7a5626613253324
mikhail.gerasim <54040496+mikhokpi@users.noreply.github.com>
2020-07-24 18:08:50 +0300

    lesson 14-15, add constructors and methods to classes, code optimization

--- sandbox/src/main/java/ru/stqa/pft/sandbox/helloWorld.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/helloWorld.java
@@ -3,22 +3,10 @@ package ru.stqa.pft.sandbox;
 public class HelloWorld {
     public static void main(String[] args) {
 
-        Square s = new Square();
-        s.l = 5;
-        System.out.println("square = " + area(s));
-        Rectangle r = new Rectangle();
-        r.a = 2;
-        r.b = 3;
-        System.out.println("rectangle = " + area(r));
+        Square s = new Square(5);
+        System.out.println("square = " + s.area());
+
+        Rectangle r = new Rectangle(2,3);
+        System.out.println("rectangle = " + r.area());
     }
-
-    public static double area(Square s) {
-        return s.l * s.l;
-    }
-
-    public static double area(Rectangle r) {
-        return r.a * r.b;
-    }
-
-
 }
